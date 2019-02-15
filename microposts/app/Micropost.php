42212573f06c34f962c231b2e1f2e4ebf8339880
houki.kanashiya <pigyybank0801@gmail.com>
2019-02-15 04:26:26 +0000

    post

---
+++ microposts/app/Micropost.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Micropost extends Model
+{
+    protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+}
